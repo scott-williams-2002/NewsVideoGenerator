88a425d4e3eaaeeaa76fe3af5e7c66ed4500b012
swilliams <smwilliams@mines.edu>
2024-01-03 12:21:56 -0500

    added a get json contents method

--- source/data_storage.py
+++ source/data_storage.py
@@ -3,21 +3,22 @@ import json
 from GPT_Wrapper import GPT_Wrapper 
 
 #saves json formatted video script contents
-def save_script_as_json(script, file_name):
+def save_script_as_json(script, script_name):
+    script_name += ".json" #script name is just a string
     new_dir_path = os.path.abspath("output_video_scripts")
     if not os.path.exists(new_dir_path):
         os.mkdir(new_dir_path)
     #checks if filename already in use and prompts overwrite or rename option
-    file_name_path = os.path.join(new_dir_path, file_name)
+    file_name_path = os.path.join(new_dir_path, script_name)
     if os.path.exists(file_name_path):
-        print(f"A file called {file_name} already exists")
-        delete_file = input(f"Do you wish to overwrite {file_name} (y/n): ")
+        print(f"A file called {script_name} already exists")
+        delete_file = input(f"Do you wish to overwrite {script_name} (y/n): ")
         # if user says yes delete old file
         if delete_file.lower() == "y":
             os.remove(file_name_path)
         #if user doesn't say yes ask for new file name
         else:
-            new_file_name = input("Enter a new json file name to write to: ")
+            new_file_name = input("Enter a new json file name to write to (EX: FILE_NAME.json)): ")
             file_name_path = os.path.join(new_dir_path, new_file_name)
 
     output_content = json.dumps(script, indent=len(script)) #formats to have propper indents for easier visualization
@@ -27,5 +28,15 @@ def save_script_as_json(script, file_name):
         out.write(output_content)
         print(f"json content written to '{file_name_path}'")
 
+#returns a json file's contents
+def get_json_contents(script_name):
+    script_name += ".json"
+    dir_path = os.path.abspath("output_video_scripts")
+    file_name_path = os.path.join(dir_path, script_name)
 
-
+    #if file exists return the json object from file and if not return nothing
+    if os.path.exists(file_name_path):
+        with open(file_name_path, 'r') as json_file:
+            json_object = json.load(json_file)
+        return json_object
+    return None
--- source/main.py
+++ source/main.py
@@ -1,14 +1,16 @@
 from gpt_handler import *
 from scraper import get_urls_from_google, get_article_text
 import time
-from os import get_terminal_size
 import sys
 import tqdm
 from TTS_Wrapper import TTS_Wrapper
 from Editor_Model import Editor_Model
+from data_storage import *
 
 def main():
+    '''
     #first call generate queries with inputted research question
+    script_name = str(input("Enter a title for this script: "))
     research_question = str(input("Enter a video topic/ research question: "))
     queries = generate_query(research_question=research_question, num_questions=5)
     
@@ -16,15 +18,18 @@ def main():
     summaries = get_summaries_from_queries(queries=queries, research_question=research_question)
     article_json = generate_article(all_summaries=summaries, research_question=research_question)
 
-    
+    #save script to file
+    save_script_as_json(article_json, script_name= script_name)
+    '''
 
-    
-    #script_text_only = ''.join(article_json['part'])
-    #tts = TTS_Wrapper()
-    #tts.set_audio_config()
-    #tts.set_voice()
-    #tts.generate_speech(script_text_only)
-    #tts.write_to_file(file_name="republican.mp3")
+    article_json = get_json_contents(script_name="iran_news")
+    script_text_only = ' '.join(article_json['part'])
+    print(script_text_only)
+    tts = TTS_Wrapper()
+    tts.set_audio_config()
+    tts.set_voice()
+    tts.generate_speech(script_text_only)
+    tts.write_to_file(file_name="republican.mp3")
     
 
    
